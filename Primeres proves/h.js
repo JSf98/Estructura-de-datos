9067a8f422c9c5e1c87691bcc18c46556bac36d3
JSf98 <jaume98sf@gmail.com>
2019-11-16 10:25:21 +0100

    Creació d'una classe i creació d'un objecte d'aquesta. Simple print.

---
+++ Primeres proves/h.js
@@ -0,0 +1,12 @@
+class H {
+
+//No fa falta ni la declaració de variables a priori. Similar a python
+  constructor() {
+    this.s = "Som una classe";
+  }
+
+  print(){
+    alert(this.s);
+  }
+  
+}
