bda4f8ba3e4a43e7463cc2140cbb62fceaaca544
JSf98 <jaume98sf@gmail.com>
2020-02-18 10:58:27 +0100

    Menu per els alumnes, podem seleccionar una estructura

---
+++ Canvas/alumne.php
@@ -0,0 +1,43 @@
+<?php session_start(); ?>
+<html>
+<head><link rel="stylesheet" type="text/css" href="div.css"></head>
+<style>
+table, th, td {
+  border: 1px solid black;
+  border-collapse: collapse;
+}
+th, td {
+  padding: 15px;
+  text-align: left;
+}
+
+table#t01 {
+  width: 100%;
+  background-color: #f1f1f1;
+}
+</style>
+<Table id="t01">
+<tr>
+    <th>Nom</th>
+    <th>Descripcio</th>
+    <th>Categoria </th>
+</tr>
+<body>
+<?php
+      include "dadescon.php";
+      if (empty($_SESSION['usuariactual'])){
+        ?><meta http-equiv="refresh" content="0; url=login.html"><?php
+      }
+
+      $cadena = "SELECT estructura.nom as nomes, url_estructura as url, estructura.descripcio as destructura, categoria.nom as nomcat, categoria.descripcio as dcategoria FROM estructura INNER JOIN categoria ON categoria.id  = estructura.categoria WHERE 1";
+      $resultat=mysqli_query($con,$cadena);
+      while ($row = mysqli_fetch_array($resultat)){
+          ?><TR>
+          <TD><a class="opcio" href="<?php echo $row['url']?>"><p><?php echo $row['nomes']?></a></TD>
+          <TD> <?php echo $row['destructura']?> </TD>
+          <TD> <?php echo $row['nomcat']?></TD><?php
+      }
+?>
+</TABLE>
+</body>
+</html>
--- Canvas/menu.php
+++ Canvas/menu.php
@@ -1,18 +1,18 @@
 <?php session_start();?>
 <html>
-<head></head>
+<head><link rel="stylesheet" type="text/css" href="div.css"></head>
 <body>
 
 <?php
     include "dadescon.php";
 
-    if(empty($_GET['usr'])){
+    if(empty($_POST['usr'])){
       ?><meta http-equiv="refresh" content="0; url=login.html"><?php
-    }else if(empty($_GET['passw'])){
+    }else if(empty($_POST['passw'])){
       ?><meta http-equiv="refresh" content="0; url=login.html"><?php
     }else{
-      $user = $_GET['usr'];
-      $pass = $_GET['passw'];
+      $user = $_POST['usr'];
+      $pass = $_POST['passw'];
       $_SESSION['usuariactual']=$user;
 
       $cadena = "SELECT usuari.user as user, usuari.password as password, usuari.tipus as tipus, opcio.url as url, opcio.titol as titol FROM usuari INNER JOIN (tipusperfil INNER JOIN (privilegi INNER JOIN opcio ON privilegi.opcio = opcio.id) ON tipusperfil.id = privilegi.perfil) ON usuari.tipus = tipusperfil.id WHERE user='".$user."' AND password = '".$pass."'";
@@ -22,15 +22,15 @@
       $row=mysqli_fetch_array($resultat);
       if (empty($row)){
           echo("Usuari o contraseña incorrecta");
-          ?><meta http-equiv="refresh" content="5; url=login.html"><?php
+          ?><meta http-equiv="refresh" content="2; url=login.html"><?php
       }else{
           ?><h2>Benvingut<?php echo " ".$row['user']?></h2><?php
           $_SESSION['admin']=$row['tipus'];
           //Imprimim la primera trama llegida
-          ?><a href="<?php echo $row['url']?>"><?php echo $row['titol']?></a><?php
+          ?><a class = "opcio" href="<?php echo $row['url']?>"><?php echo $row['titol']?></a><?php
           //Bucle de les opcions restants
           while ($row = mysqli_fetch_array($resultat)){
-              ?><a href="<?php echo $row['url']?>"><p><?php echo $row['titol']?></a><?php
+              ?><a class = "opcio" href="<?php echo $row['url']?>"><p><?php echo $row['titol']?></a><?php
           }
       }
   }?>
